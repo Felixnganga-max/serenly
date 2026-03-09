656fc3d5be7f8709ff8f7a8f4afbcbb4edc4bd55
Felix Ngunga <felixngunga22@gmail.com>
2026-03-09 03:55:04 +0300

    vercel

--- serenly/src/App.jsx
+++ serenly/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import Home from "./pages/Home";
 import { Route, Routes } from "react-router-dom";
 import Solutions from "./pages/Solutions";
